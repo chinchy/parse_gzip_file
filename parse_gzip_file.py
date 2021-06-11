aca65fa8950723ca19edb2dab8d71edca0188a32
Petrov Ilya <petrov_ii@kgiop.gov.spb.ru>
2021-06-11 11:52:07 +0300

    Init commit

---
+++ parse_gzip_file.py
@@ -0,0 +1,40 @@
+import gzip
+from os.path import exists
+
+
+def parse_gzip_file(path: str) -> dict:
+    data = {}
+
+    if not exists(path) and path.endswith('.gz'):
+        print(f'Файл {path} не найден!')
+        return data
+
+    with gzip.open(path, mode='rt') as f:
+        key = ""
+        for line in f:
+            if not line.strip():  # Защита от пустой строки
+                key = ""
+                continue
+            elif line.count(':'):  # Обработка строки с ключом
+                key, value = line.split(':')
+                value = value.strip()
+            else:                  # Обработка строки без ключа
+                value = line.strip()
+
+            if key in data:  # Сохранение многострочности
+                value = '\n' + value
+
+            if key and not value.startswith('#'):  # Защита от комментариев
+                data[key] = data.get(key, '') + value
+    return data
+
+
+def load_data(path: str):
+    parsed_data = parse_gzip_file(path)
+
+    for document in parsed_data:
+        pass
+        # load document to database (or do something else)
+
+
+print(parse_gzip_file("example.gz"))
